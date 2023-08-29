c2bbbb677be00a98c11a37db8b98a6562e2941ae
phuriungung <puriung08@gmail.com>
2023-08-29 16:39:30 +0700

    Initial Commit

---
+++ writableswiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  writableswiftui
+//
+//  Created by minto on 29/8/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
